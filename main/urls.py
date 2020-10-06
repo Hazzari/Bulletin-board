8f0fa196d5016996d16f9fb003996f224d3e1bb8
Hazzari <dev.aleksan@gmail.com>
2020-10-06 23:56:22 +0300

    feat(main): Добавил Change user info, Обработчик и шаблон.

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, BBlogoutView, profile, ViewTest
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('accounts/profile/', profile, name='profile'),
     path('accounts/login', BBLoginView.as_view(), name='login'),
-    path('accounts/logout', BBlogoutView.as_view(), name='logout'),
+    path('accounts/logout', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 def index(request):
@@ -22,10 +29,27 @@ class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
 
-class BBlogoutView(LogoutView):
+class BBLogoutView(LogoutView):
     template_name = 'main/logout.html'
 
 
 @login_required
 def profile(request):
     return render(request, 'main/profile.html')
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
