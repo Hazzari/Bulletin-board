5c9035bf0316e15b9cb65b698eec51928b60db61
Hazzari <dev.aleksan@gmail.com>
2020-10-06 15:56:08 +0300

    feat(main): Добавил модель пользователь.             зарегистрировал в админке.             создал миграцию.

--- bboard/settings.py
+++ bboard/settings.py
@@ -92,6 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'main.AdvUser'
 
 LANGUAGE_CODE = 'ru'
 TIME_ZONE = 'UTC'
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AdvUser
+
+
+@admin.register(AdvUser)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
