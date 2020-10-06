4637d67bccfb94588341bf197b4ffe72f7001151
Hazzari <dev.aleksan@gmail.com>
2020-10-06 16:28:55 +0300

    feat(main): Добавил Login, вариант отображения в шаблонах в зависимости от аутентификации профиль/регистрация

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
@@ -14,3 +15,7 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
