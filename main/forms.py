9b879164bc47f6ce8bc8066c327df20eda8fff28
Hazzari <dev.aleksan@gmail.com>
2020-10-22 23:36:01 +0300

    feat(main): Добавил форму для комментария

--- main/forms.py
+++ main/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.forms import inlineformset_factory
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
+from captcha.fields import CaptchaField
 
 from .apps import user_registered
-from .models import AdvUser, SuperRubric, SubRubric, Bb, AdditionalImage
+from .models import AdvUser, SuperRubric, SubRubric, Bb, AdditionalImage, Comment
 
 
 class BbForm(forms.ModelForm):
@@ -76,3 +77,19 @@ class SearchForm(forms.Form):
 
 
 AIFormSet = inlineformset_factory(Bb, AdditionalImage, fields='__all__')
+
+
+class UserCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('is_active',)
+        widgets = {'bb': forms.HiddenInput}
+
+
+class GuestCommentForm(forms.ModelForm):
+    captcha = CaptchaField(label='Введите текст с картинки', error_messages={'invalid': 'Неправильный текст'})
+
+    class Meta:
+        model = Comment
+        exclude = ('is_active',)
+        widgets = {'bb': forms.HiddenInput}
