8a7349557399fd6651bddcff70dbcb3d223fb57d
Aleksan <Dev.aleksan@gmail.com>
2021-02-08 21:53:54 +0300

    feat(api): Добавил приложение api

--- bboard/settings.py
+++ bboard/settings.py
@@ -52,18 +52,23 @@ INSTALLED_APPS = [
 
     # Created apps
     'main.apps.MainConfig',
+    'api.apps.ApiConfig'
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGINAL_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = 'bboard.urls'
 
